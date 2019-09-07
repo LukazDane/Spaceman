a587b88c7fe822e2dc6e50f02e35696b1176d2ec
Luke <deandrevidal@aol.com>
2019-09-07 16:06:30 -0700

    set up win/loss strings to print

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
 import random
+
+spaceman_winner_string = """
+(\_/)
+(0_0)
+(u u)
+(u u)
+(u u)
+"""
+spaceman_loser_string = """
+(\_/)
+(X_X)
+(u u)
+(u u)
+(u u)
+
+"""
+spaceman_loser_list = spaceman_loser_string.split('\n')
+body_part_count = len(spaceman_loser_list)
