2e332a86642e2006156bf4b78355cae737e149fa
Luke <deandrevidal@aol.com>
2019-09-07 21:35:33 -0700

    set up test list

--- app.py
+++ app.py
@@ -17,3 +17,14 @@ spaceman_loser_string = """
 """
 spaceman_loser_list = spaceman_loser_string.split('\n')
 body_part_count = len(spaceman_loser_list)
+
+
+def main():
+    # sets up  list of themed words
+    secret_string = random.choice(['eevee', 'mareep', 'pikachu'])
+    secret_word = list(secret_string)
+    secret_set = (secret_word)
+    # game setup
+    displayed_word_list = list("_" * len(secret_word))
+    turns_remaining = body_part_count
+    guess_history_string = ""
